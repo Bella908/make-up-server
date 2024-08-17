56e5d315f0a515586eda6e460eb929de34b69f16
bella908 <mumtahina efa8@gmail.com>
2024-08-17 13:18:23 +0600

    search

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+const app = express();
 // middleware
 app.use(cors());
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ld1lprp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
@@ -91,15 +91,15 @@ async function run() {
     
   }
 }
-run().catch(console.dir);
 
 
 
 
 app.get('/',(req,res) =>{
-    res.send('Simple')
+  res.send('Simple')
 })
 
 app.listen(port,() =>{
-    console.log(`simple,${port}`)
+  console.log(`simple,${port}`)
 })
+run().catch(console.dir);
